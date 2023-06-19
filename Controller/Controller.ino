b5322e5bdb3440576e74a3a2955c096f5a958bd7
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-06-19 13:37:57 -0400

    Adding our own API to the code base

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -39,22 +39,96 @@ void setup()
 void logHatCoordinate()
 {
     // Left Hat (Left Joystick)
-    Serial.println("Left Hat (Left Joystick):");
+    Serial.println("-- Joystick --");
+    Serial.print("| Left Joystick:");
     Serial.print("X: ");
     Serial.print(PS4.getAnalogHat(LeftHatX));
-    Serial.print("\t");
+    Serial.print(" -- ");
     Serial.print("Y: ");
     Serial.print(PS4.getAnalogHat(LeftHatY));
-    Serial.print("\r\n");
+    Serial.print("\r"); // ?
 
     // Right Hat (Right Joystick)
-    Serial.println("Right Hat (Right Joystick):");
+    Serial.print("\t| Right Joystick:");
     Serial.print("X: ");
     Serial.print(PS4.getAnalogHat(RightHatX));
     Serial.print("\t");
     Serial.print("Y: ");
     Serial.print(PS4.getAnalogHat(RightHatY));
-    Serial.print("\r\n");
+    Serial.print("|\r");
+}
+
+void disconnectController()
+{
+    Serial.println("Controller disconnected");
+    PS4.disconnect();
+}
+
+void setRumble(int level)
+{
+    /**
+     * @brief Set rumble (vibration) of the controller
+     * @param level: 0 (stop), 1 (weak), 2 (strong)
+     * NOTE: see setRumbleOn() in PS4Parser.h
+     */
+    if (level == 0)
+    {
+        PS4.setRumbleOff();
+    }
+    else if (level == 1)
+    {
+        PS4.setRumbleOn(RumbleLow);
+    }
+    else if (level == 2)
+    {
+        PS4.setRumbleOn(RumbleHigh);
+    }
+}
+
+void setRumble(int level, int duration)
+{
+    /**
+     * @brief Set rumble (vibration) of the controller
+     * @param level: 0 (stop), 1 (weak), 2 (strong)
+     * @param duration: duration in milliseconds
+     */
+    setRumble(level);
+    delay(duration);
+}
+
+void setLED_Color(ColorsEnum color)
+{
+    /**
+     * @brief Set the color of the LED
+     * @param color: color of the LED
+     * OPTIONS: Red, Green, Blue, Yellow, Lightblue, Purple, White, Off
+     */
+    PS4.setLed(color);
+}
+
+
+void setLED_RGB(uint8_t r, uint8_t g, uint8_t b)
+{
+    /**
+     * @brief Set the color of the LED
+     * @param r: red channel (0-255)
+     * @param g: green channel (0-255)
+     * @param b: blue channel (0-255)
+     * NOTE: see setLed() in PS4Parser.h
+     */
+    PS4.setLed(r, g, b);
+}
+
+void setLED_Flashing(uint16_t period, unsigned int duration)
+{
+    /**
+     * @brief Set the LED to flash
+     * @param period: period of flashing in milliseconds
+     * @param duration: duration of flashing in milliseconds
+     * NOTE: see setLedFlash() in PS4Parser.h
+     */
+    PS4.setLedFlash(period, period);
+    delay(duration);
 }
 
 void loop()
